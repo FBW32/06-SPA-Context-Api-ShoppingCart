b5b0389293d4baabc80feb5a388dfb25bb25a535
frausafiye <s_gulenc@outlook.com>
2020-11-24 09:11:34 +0100

    added solution

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
+import Container from "./Container";
+import Product from "./Product";
+import Cart from "./Cart";
 
 export default class App extends React.Component {
   render() {
 
     return (
-      <div>
-     	<h1>My React Project</h1>
-      </div>
+      <Container>
+        <ul>
+          <Product/>
+        </ul>
+        <hr/>
+         <ul>
+        <Cart/>
+        </ul>
+      </Container>
     );
   }
 }
